972ecc455395c4db67a5c3edadaa0d9b180df2f2
Daniel Wong <dsw2107@gmail.com>
2023-05-04 02:21:35 -0700

    Add support for sofle_choc
    Adding support for Sofle Choc by Brian Low

---
+++ keyboards/sofle_choc/keymaps/vial/rules.mk
@@ -0,0 +1,37 @@
+TRI_LAYER_ENABLE = yes
+
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+OLED_ENABLE = yes
+
+# Enable per-key lighting
+RGB_MATRIX_ENABLE = yes
+
+# Enable audio keys and system control
+EXTRAKEY_ENABLE = yes
+
+# Enable link time optimization
+# Trade longer compile time for smaller firmware size
+LTO_ENABLE = yes
+
+# Try and save some space
+QMK_SETTINGS = no
+# TAP_DANCE_ENABLE = no
+# COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+
+# CONSOLE_ENABLE = no
+# COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE = no
+
+AVR_USE_MINIMAL_PRINTF = yes
+
+# Enable use of Vial
+# Similar to VIA
+# https://get.vial.today/
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
